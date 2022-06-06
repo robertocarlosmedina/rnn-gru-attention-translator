5ba531fd1b932e4233edf9502cff28c1d9767c70
Roberto <robertocarlosmedina.dev@gmail.com>
2022-06-06 17:35:02 -0100

    feat: Implementation of the model Integration of the Dataset

---
+++ decoder_rnn.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, device):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.device = device
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=self.device)
